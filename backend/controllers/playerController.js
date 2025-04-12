121577b98ab5e00831adfcfcba2615818e9f22ef
Aniket <aniketto63@gmail.com>
2025-04-12 18:08:58 +0530

    improved login and signup

--- backend/controllers/playerController.js
+++ backend/controllers/playerController.js
@@ -1,32 +1,30 @@
+const userarr = [];
+const sign = (req, res) => {
+  console.log("sign");
+  const { name, password } = req.body;
+  const existing = userarr.find((user) => user.name === name);
+  if (existing) return res.status(400).json({ message: " already user" });
+  console.log("sign", req.body);
+  userarr.push({ name, password });
+  return  res.status(200).json({ m: "sign secussfull" });
+};
 
+const login = (req, res) => {
+  const { name, password } = req.body;
+  const existing = userarr.find((user) => user.name === name);
 
+  if (!existing) return res.status(400).json({ message: "no user" });
+  if (existing.password !== password)
+    return res.status(400).json({ message: "wrong password" });
+  req.session.vaild = true;
+  console.log(req.body);
+  return res.status(200).json({ message: "login successful" });
+};
 
-const sign = (req,res)=>{
-    console.log("sign")
-    const {name,password} = req.body;
-//    const vaild =  req.userarr.find(name);
-//     if(vaild) res.status(400).josn({ message: " already user"});
+const logout = (req, res) => {
+  console.log("logout");
+  req.session.destroy();
+   return res.status(200).json({ message: "Logged out" });
+};
 
-    req.userarr= {name,password};
-    
-    
-   }
-
-   const login = (req,res)=>{
-    const {name,password} = req.body;
-    // const valid = req.userarr.find(user => user.name === name);
-
-    //   if(!vaild) res.status(400).josn({ message: "no user"});
-    //   if(vaild[0].password!==password)  res.status(400).josn({ message: "wrong password"});
-       req.session.vaild=true;
-       console.log("clogn")
-      
-   }
-
-   const logout = (req, res) => {
-    console.log("logout")
-    req.session.destroy();
-    res.status(200).json({ message: 'Logged out' });
-  };
-
-  module.exports = { sign, login, logout };
+module.exports = { sign, login, logout };
