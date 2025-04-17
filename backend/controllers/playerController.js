63fbf9a4c0add13d03b717ccf46367d2a854eb87
Aniket <aniketto63@gmail.com>
2025-04-17 10:10:59 +0530

    chess

--- backend/controllers/playerController.js
+++ backend/controllers/playerController.js
@@ -6,19 +6,19 @@ const sign = (req, res) => {
   if (existing) return res.status(400).json({ message: " already user" });
   console.log("sign", req.body);
   userarr.push({ name, password });
-  return res.status(200).json({ m: "sign secussfull" });
+  return   res.redirect("http://localhost:5000/login.html");
 };
 
 const login = (req, res) => {
   const { name, password } = req.body;
   const existing = userarr.find((user) => user.name === name);
 
-  if (!existing) return res.status(400).json({ message: "no user" });
+  if (!existing) return res.redirect("http://localhost:5000/sign.html");
   if (existing.password !== password)
     return res.status(400).json({ message: "wrong password" });
   req.session.vaild = true;
   console.log(req.body);
-  return res.status(200).json({ message: "login successful" });
+  return res.redirect("http://localhost:5000/home.html") ;
 };
 
 const logout = (req, res) => {
